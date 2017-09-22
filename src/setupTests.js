efcd157520edb5703f4fcc9e77004523cab8c394
Rickard Laurin <rickard.laurin@gmail.com>
2017-09-22 10:04:04 +0200

    Added test mocks

---
+++ src/setupTests.js
@@ -0,0 +1,28 @@
+// Mock localStorage
+const localStorageMock = {
+  getItem: jest.fn(param => param),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn()
+}
+
+global.localStorage = localStorageMock
+
+// Mocking the global.fetch included in React Native
+window.fetch = jest.fn()
+
+// Helper to mock a success response (only once)
+fetch.mockResponseSuccess = body => {
+  fetch.mockImplementationOnce(() =>
+    Promise.resolve({ json: () => Promise.resolve(body) })
+  )
+}
+
+fetch.mockResponseSuccessWithError = body => {
+  fetch.mockImplementationOnce(() => Promise.resolve(body))
+}
+
+// Helper to mock a failure response (only once)
+fetch.mockResponseFailure = error => {
+  fetch.mockImplementationOnce(() => Promise.reject(error))
+}
