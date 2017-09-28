4a1fccd192753d3c41ff25adba1d55dc7c8d5aba
Rickard Laurin <rickard.laurin@gmail.com>
2017-09-28 15:14:29 +0200

    Only display users that have been active in the last hour

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,4 +1,5 @@
 import './Users.css'
+import moment from 'moment'
 import React from 'react'
 import Gravatar from 'components/Gravatar'
 
@@ -9,12 +10,24 @@ const Users = ({ users }) => {
 
   return (
     <ul className="Users">
-      {users.map(user => (
-        <li className="Users__user" key={user.id}>
-          <Gravatar email={user.email} size={30} />
-          <div className="Users__email">{user.email}</div>
-        </li>
-      ))}
+      {users
+        .slice()
+        .filter(user => {
+          return (
+            user.lastPlay !== 0 &&
+            moment().diff(moment(user.lastPlay), 'minutes') <= 60
+          )
+        })
+        .sort((a, b) => b.lastPlay - a.lastPlay)
+        .map(user => (
+          <li className="Users__user" key={user.id}>
+            <Gravatar email={user.email} size={30} />
+            <div className="Users__email">{user.email}</div>
+            <div className="Users__lastPlay">
+              {moment(user.lastPlay).fromNow()}
+            </div>
+          </li>
+        ))}
     </ul>
   )
 }
--- src/components/Users/__tests__/Users.spec.js
+++ src/components/Users/__tests__/Users.spec.js
@@ -1,14 +1,23 @@
+import mockdate from 'mockdate'
 import React from 'react'
 import { shallow } from 'enzyme'
-import toJson from 'enzyme-to-json'
 import Users from '../Users'
 
 describe('components/Users', () => {
   let component
 
   beforeEach(() => {
+    mockdate.set('2017-09-28 12:00')
+
     component = shallow(
-      <Users users={[{ email: 'cookie@monster.com', id: '1' }]} />
+      <Users
+        users={[
+          { email: 'cookie@monster.com', id: '1', lastPlay: 0 },
+          { email: 'count@vancount.com', id: '2', lastPlay: 1505599875714 },
+          { email: 'big@bird.com', id: '3', lastPlay: 1506599875714 },
+          { email: 'big@bird.com', id: '3', lastPlay: 1506599873714 }
+        ]}
+      />
     )
   })
 
@@ -20,7 +29,7 @@ describe('components/Users', () => {
     expect(component.type()).toBeNull()
   })
 
-  it('renders Users', () => {
-    expect(toJson(component)).toMatchSnapshot()
+  it('renders Users and filters users without play and no plays in the last hour', () => {
+    expect(component).toMatchSnapshot()
   })
 })
