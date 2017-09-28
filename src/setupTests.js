f79458ab08fa939aed13fcbf261e71886cde5c5c
Rickard Laurin <rickard.laurin@gmail.com>
2017-09-28 15:27:01 +0200

    Updated to React 16

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,8 @@
 import serializer from 'enzyme-to-json/serializer'
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
+Enzyme.configure({ adapter: new Adapter() })
 expect.addSnapshotSerializer(serializer)
 
 // Mock localStorage
