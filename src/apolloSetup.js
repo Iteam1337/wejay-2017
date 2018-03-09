adffbdee9595a23b4bf28196140ea50ca3f67fd4
Rickard Laurin <rickard.laurin@gmail.com>
2018-03-09 14:04:10 +0100

    fix: split ws and http graphql

--- src/apolloSetup.js
+++ src/apolloSetup.js
@@ -1,18 +1,29 @@
 import ApolloClient from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
-import { ApolloLink } from 'apollo-link'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { WebSocketLink } from 'apollo-link-ws'
 import { SubscriptionClient } from 'subscriptions-transport-ws'
+import { split } from 'apollo-link'
+import { getMainDefinition } from 'apollo-utilities'
 
 const httpLink = new HttpLink({ uri: process.env.REACT_APP_GRAPHQL_URL })
-const wsClient = new SubscriptionClient(process.env.REACT_APP_SUB_URL || '', {
+const wsClient = new SubscriptionClient(process.env.REACT_APP_SUB_URL, {
   reconnect: true,
 })
 
-const subscriptionLink = new WebSocketLink(wsClient)
+const wsLink = new WebSocketLink(wsClient)
+
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLink
+)
 
 export const client = new ApolloClient({
-  link: ApolloLink.from([httpLink, subscriptionLink]),
+  link,
   cache: new InMemoryCache(),
 })
